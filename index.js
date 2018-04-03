23ba6847656e3f57dac45cb4be10d89313bb328f
Rebecca Porter <ra.porter19@gmail.com>
2018-04-03 14:19:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
+  console.log(`It's ${days} days until ${holiday}!`)
   return `It's ${days} days until ${holiday}!`
 }
